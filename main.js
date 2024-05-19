104fa0e77c1b385066e31a8bb97f91618948c51f
JUANTITO77 <jelvistq77@gmail.com>
2024-05-18 21:27:21 -0500

    Descripción de los cambios realizados

--- main.js
+++ main.js
@@ -10,8 +10,30 @@ cerrar.addEventListener('click',()=>{
     menu.classList.remove('visible');
 })
 
+// slider de promociones
 
+const slider = document.querySelector('.slider-contenedor');
+const sliderIndividual = document.querySelectorAll('.contenido-slider'); // Cambiado a querySelectorAll
+let contador = 0;
+let width = sliderIndividual[0].clientWidth;
+let intervalo = 3000;
 
+window.addEventListener('resize', () => {
+    width = sliderIndividual[0].clientWidth; // Recalcula el ancho en caso de redimensionar la ventana
+});
+
+setInterval(() => {
+    slides();
+}, intervalo);
+
+function slides() {
+    contador++;
+    if (contador >= sliderIndividual.length) {
+        contador = 0;
+    }
+    slider.style.transform = 'translate(' + (-width * contador) + 'px)';
+    slider.style.transition = 'transform 0s';
+}
 
 
 
